21ed648ebc8245083a856130b0890518bd234301
rhobbikhaidir <rhobbie09@gmail.com>
2022-12-10 21:10:36 +0700

    initial baseUrl

---
+++ src/service/api.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://env-9245888.user.cloudjkt01.com/v1/business",
+});
+
+const service = {
+  getAllFood: async (limit = 5, page = 1, categories, price) => {
+    try {
+      const response = await api.get("/search", {
+        params: {
+          limit,
+          page,
+          categories,
+          price,
+        },
+      });
+
+      return response.data;
+    } catch (err) {
+      return err;
+    }
+  },
+};
+
+
+
+export default service;
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,30 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import service from "../service/api";
+
+export const getAllFood = createAsyncThunk(
+  "/getAllFood",
+  async (params, { rejectWithValue }) => {
+    try {
+      const response = await service.getAllFood(
+        params.limit,
+        params.page,
+        params.categories,
+        params.price
+      );
+      console.log(response);
+      return response;
+    } catch (error) {
+      rejectWithValue(error);
+    }
+  }
+);
 
 const initialState = {
-    bussinesItems: [],
-  };
-  
-  const bussinesSlicer = createSlice({
-    name: 'sliceUsers',
-    initialState: { ...initialState },
-    reducers: {
-      setChangeVal: (state, action) => {
-        state[action.payload.key] = action.payload.value;
-      },
+  bussinesItems: [],
+};
+
+const bussinesSlicer = createSlice({
+  name: "sliceUsers",
+  initialState: { ...initialState },
+  reducers: {
+    setChangeVal: (state, action) => {
+      state[action.payload.key] = action.payload.value;
     },
-    extraReducers: (builder) => {
-    //   builder.addCase(getUsers.fulfilled, (state, action) => {
-    //     // console.log('data balikan dari api :', action.payload);
-    //     state.values = action.payload;
-    //   });
+  },
+  extraReducers: (builder) => {
+      builder.addCase(getAllFood.fulfilled, (state, action) => {
+        console.log('data balikan dari api :', action.payload);
+        // state.values = action.payload;
+      });
     //   builder.addCase(findUsers.fulfilled, (state, action) => {
     //     state.findUser = action.payload;
     //   });
-  
     //   builder.addCase(getUserRepo.fulfilled, (state, action) => {
     //     state.repoUser = action.payload;
     //   });
-    },
-  });
-  export const {setChangeVal} = bussinesSlicer.actions
-  export default bussinesSlicer.reducer;
+  },
+});
+export const { setChangeVal } = bussinesSlicer.actions;
+export default bussinesSlicer.reducer;
