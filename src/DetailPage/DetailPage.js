48904e8943b9c9ff96b4b771783e476c42e88c6d
AinunSeptian <ainunseptian79@gmail.com>
2022-12-10 20:33:18 +0700

    progress detail page

--- src/DetailPage/DetailPage.js
+++ src/DetailPage/DetailPage.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const DetailPage = () => {
   return (
-    <div>DetailPage</div>
+    <section>
+        <div className='bg-black h-[400px]'>
+            <div className='container mx-auto flex flex-col'>
+                <h1 className='text-white text-[42px] font-bold'>Amici’s East Coast Pizzeria at</h1>
+                <h1 className='text-white text-[42px] font-bold'>CloudKitchens SOMA</h1>
+            </div>
+        </div>
+    </section>
   )
 }
 
