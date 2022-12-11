03eedf0513600b591ff064b971919e67f834dbdf
rhobbikhaidir <rhobbie09@gmail.com>
2022-12-11 15:51:56 +0700

    fixing conflict and add limit in pagination

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,41 +1,44 @@
-import React, { useState } from 'react';
-import Search from '../assets/icons/search.png';
+import React from "react";
+import Search from "../assets/icons/search.png";
 
-const Navbar = () => {
-  const [item, setItem] = useState('');
-  const [location, setLocation] = useState('');
-
+const Navbar = ({
+  onChangeFood,
+  onChangeLocation,
+  valueLocation,
+  valueFood,
+  onSubmit,
+}) => {
   return (
     <>
       <section>
-        <div className='bg-[#DC1919] w-full h-auto px-4 py-8 flex justify-center items-center'>
-          <div className='w-[800px] flex justify-end items-center'>
-            <form>
+        <div className="bg-[#DC1919] w-full h-auto px-4 py-8 flex justify-center items-center">
+          <div className="w-[800px] flex justify-end items-center">
+            <form onSubmit={onSubmit}>
               <input
-                type='text'
-                placeholder='Searching something ...'
-                name='item'
-                value={item}
+                type="text"
+                placeholder="Searching something ..."
+                name="item"
+                value={valueFood}
                 required
-                onChange={(e) => setItem(e.target.value)}
-                className='w-[300px] outline-none py-2 rounded-l-md border-r-2 border-gray-300 text-center'
+                onChange={onChangeFood}
+                className="w-[300px] outline-none py-2 rounded-l-md border-r-2 border-gray-300 text-center"
               />
               <input
-                type='text'
-                placeholder='Location ...'
-                name='location'
-                value={location}
-                onChange={(e) => setLocation(e.target.value)}
-                className='w-[300px] outline-none py-2 text-center'
+                type="text"
+                placeholder="Location ..."
+                name="location"
+                value={valueLocation}
+                onChange={onChangeLocation}
+                className="w-[300px] outline-none py-2 text-center"
               />
               <button
-                type='submit'
-                className='bg-blue-800 px-4 py-3 text-center rounded-r-md text-white text-base'
+                type="submit"
+                className="bg-blue-800 px-4 py-3 text-center rounded-r-md text-white text-base"
               >
                 <img
                   src={Search}
-                  alt='logo-search'
-                  className='w-[16px] h-[16px] object-contain'
+                  alt="logo-search"
+                  className="w-[16px] h-[16px] object-contain"
                 />
               </button>
             </form>
--- src/landingPage/LandingPage.js
+++ src/landingPage/LandingPage.js
@@ -1,84 +1,99 @@
-import React, { useEffect } from 'react';
-import Navbar from '../Navbar/Navbar';
-import { useDispatch, useSelector } from 'react-redux';
-import { getAllFood } from '../store/reducer';
-import Loading from '../Loading/Loading';
-import FoodItems from './FoodItems';
-import Select from 'react-select';
+import React, { useEffect } from "react";
+import Navbar from "../Navbar/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllFood } from "../store/reducer";
+import Loading from "../Loading/Loading";
+import FoodItems from "./FoodItems";
+import Select from "react-select";
+import { useState } from "react";
+import Pagination from "./Pagination";
 
 const LandingPage = () => {
+  const [activePage, setActivePage] = useState(1);
+  const { loader, bussinesItems, totalCount } = useSelector((state) => state);
+  const [limit, setLimit] = useState(5);
+
   const categories = [
-    { title: 'Sandwich' },
-    { title: 'Breakfast & Brunch' },
-    { title: 'Mexican' },
-    { title: 'Chinese' },
+    { title: "Sandwich" },
+    { title: "Breakfast & Brunch" },
+    { title: "Mexican" },
+    { title: "Chinese" },
+    { title: "Pizza" },
+
   ];
 
   const valueSelect = [
-    { value: '5', label: '5' },
-    { value: '10', label: '10' },
-    { value: 'All', label: 'All' },
+    { value: 5, label: "5" },
+    { value: 10, label: "10" },
+    { value: totalCount, label: "All" },
   ];
 
-  const { loader, bussinesItems } = useSelector((state) => state);
-  console.log(loader, bussinesItems, 'ini loader');
-  // const [page, setPage] = useState(1)
   const dispatch = useDispatch();
 
-  const getAllFoodBussines = (page, price = '', categories = '') => {
-    dispatch(getAllFood({ limit: 4, page: page, price, categories }));
+  const getAllFoodBussines = (price = "", categories = "", term= "") => {
+    dispatch(getAllFood({ limit: limit, page: activePage, price, categories, term }));
   };
 
   useEffect(() => {
-    getAllFoodBussines(1);
-  }, [dispatch]);
+    getAllFoodBussines();
+  }, [dispatch, limit, activePage]);
+
+
+
+  let totalPost =  totalCount / limit ;
+
+
+  console.log(totalPost, 'total cuy')
+  const paginate = (pageNumber) => setActivePage(pageNumber);
+  const paginateFront = () => setActivePage(activePage + 1);
+  const paginateBack = () => setActivePage(activePage - 1);
 
   return (
     <>
       <div>
         <Navbar />
         {/* wrapper jumbo */}
-        <div className='flex h-full'>
-          <div className='w-[250px] h-[80rem] bg-black px-4 py-4'>
-            <h1 className='text-base font-semibold text-white text-left uppercase mb-10'>
+        <div className="flex h-full">
+          <div className="w-[250px] h-[80rem] bg-black px-4 py-4">
+            <h1 className="text-base font-semibold text-white text-left uppercase mb-10">
               Filters
             </h1>
             {/* button price */}
-            <div className='flex justify-center items-center mt-3 mb-6'>
-              <button className='bg-blue-600 px-4 py-2 border-white border-r-2 rounded-l-full'>
-                <span className='text-lg font-semibold text-center text-white'>
+            <div className="flex justify-center items-center mt-3 mb-6">
+              <button className="bg-blue-600 px-4 py-2 border-white border-r-2 rounded-l-full">
+                <span className="text-lg font-semibold text-center text-white">
                   $
                 </span>
               </button>
-              <button className='bg-blue-600 px-4 py-2 border-white border-r-2'>
-                <span className='text-lg font-semibold text-center text-white'>
+              <button className="bg-blue-600 px-4 py-2 border-white border-r-2">
+                <span className="text-lg font-semibold text-center text-white">
                   $$
                 </span>
               </button>
-              <button className='bg-blue-600 px-4 py-2'>
-                <span className='text-lg font-semibold text-center text-white'>
+              <button className="bg-blue-600 px-4 py-2">
+                <span className="text-lg font-semibold text-center text-white">
                   $$$
                 </span>
               </button>
-              <button className='bg-blue-600 px-4 py-2 rounded-r-full border-white border-l-2'>
-                <span className='text-lg font-semibold text-center text-white'>
+              <button className="bg-blue-600 px-4 py-2 rounded-r-full border-white border-l-2">
+                <span className="text-lg font-semibold text-center text-white">
                   $$$$
                 </span>
               </button>
             </div>
             {/* Category*/}
-            <h1 className='text-xl font-semibold uppercase text-white text-left mt-10'>
+            <h1 className="text-xl font-semibold uppercase text-white text-left mt-10">
               Category
             </h1>
             {/* button categories */}
-            <div className='mt-3'>
+            <div className="mt-3">
               {categories.map((val, i) => {
                 return (
                   <button
                     key={i}
-                    className='bg-[red] px-4 py-2 rounded-full text-center mb-2 mr-2'
+                    className="bg-[red] px-4 py-2 rounded-full text-center mb-2 mr-2"
                   >
-                    <p className='text-center text-sm font-normal text-white'>
+                    <p className="text-center text-sm font-normal text-white">
                       {val.title}
                     </p>
                   </button>
@@ -88,9 +103,9 @@ const LandingPage = () => {
           </div>
 
           {/* wrapper foodItem */}
-          <div className='w-full flex justify-center items-start bg-gray-400'>
-            <div className='flex flex-col mt-4'>
-              <Select options={valueSelect} className='w-[100px]' />
+          <div className="w-full flex justify-center items-start bg-gray-400">
+            <div className="flex flex-col mt-4">
+              <Select options={valueSelect} className="w-[100px]" onChange={(e) => setLimit(e.value)}  defaultValue={valueSelect[0]} />
               {bussinesItems?.map((item, index) => {
                 return (
                   <FoodItems
@@ -103,10 +118,23 @@ const LandingPage = () => {
                   />
                 );
               })}
+              <div className="text-center py-5">
+                <Pagination
+                  postsPerPage={limit}
+                  totalPosts={totalPost}
+                  totalCount={totalCount}
+                  paginate={paginate}
+                  currentPage={activePage}
+                  paginateBack={paginateBack}
+                  paginateFront={paginateFront}
+                />
+              </div>
             </div>
           </div>
         </div>
         {loader && <Loading />}
+
+        <div></div>
       </div>
     </>
   );
